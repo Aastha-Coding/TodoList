f9d01dcf16d79ac07b17d031fc5882b155d44d50
Aastha-Coding <67990914+Aastha-Coding@users.noreply.github.com>
2020-07-08 08:48:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import {
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
+
+export default class Example extends Component {
+  async createPDF() {
+    let options = {
+      html: '<h1>PDF TEST</h1>',
+      fileName: 'test',
+      directory: 'Documents',
+    };
+
+    let file = await RNHTMLtoPDF.convert(options).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
+    // console.log(file.filePath);
+    alert(file.filePath);
+  }
+
+  render() {
+    return (
+      <View>
+        <TouchableHighlight onPress={this.createPDF}>
+          <Text>Create PDF</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
